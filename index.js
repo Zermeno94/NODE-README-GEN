0e22b03df051079a98aef6fbb6889893ad519d48
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-02 20:13:15 -0500

    worked on index.js questions

--- index.js
+++ index.js
@@ -1,3 +1,89 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 // imports inquirer & fs
+
+// questions that will be asked
+const questions = [
+    { // project title 
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+
+    { //description 
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    
+    { //  installation instructions,  and test instructions
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+
+    { //usage information
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+
+    { //contribution guidelines
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+
+    { //usage
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+
+    { //license
+        type: "input",
+        name: "license",
+        message: "Please provide the project licence or your badge link"
+    },
+
+    { // badge
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+
+    { //test
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+
+    { //github username 
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+
+    { //github rep
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
+
+
+//TODO
+
+//THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//WHEN I enter my project title
+//THEN this is displayed as the title of the README
+//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+//THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+//WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
