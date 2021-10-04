b62c36ea9c055f09376001a17a3bcf79f41263bf
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-03 23:53:34 -0500

    fixing issues in index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 // TODO: Include packages needed for this application
-// Exporting  & Internal packages
-const inquirer = require("inquirer");
-const fs = require("fs");
-const utils = require("util");
-const generateMarkdown = require('./utils/generateMarkdown.js');
-const writeFilesync = utils.proisify(fs.writeFile);
-
-
+// Export & Internal packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-// my array was functioning the way i thought it would like in mini-project
-// so i decided to create object for questions 
-
+// I didn't like the array approach, so i moved to object instead
 const questions = {
     title: 'What is the title of your project?',
 
@@ -23,122 +17,100 @@ const questions = {
 
     license: 'Do you want to include a license in this project?',
 
-    contributing: 'Please enter any contribution guidelines you have for the project.',
+    contributing: 'Please enter contributors for the project.',
 
     tests: 'Please provide any test instructions you have for the project.',
 
     email: 'What is your email address?',
 
-    github: 'What is your GitHub username?',
+    github: 'What is your GitHub Id?',
 
-    repository: 'Please provide link to your GitHub repository?'
+    repository: 'Please provide link to your GitHub repository?',
 };
 
-// Inquirer prompts(questions) to user
-// pulled from bonus mini project
-const promptUser = () => {
+ // questions that will be prompt to user
+
+const promptUser = () => { // this was pulled from bonus solved mini-project
     return inquirer.prompt([
         
-        { // project title 
-            type: "input",
-            name: "title",
+        { //title
+            type: 'input',
+            name: 'itle',
             message: questions.title
         },
-    
-        { //description 
-            type: "input",
-            name: "description",
+
+        { //description
+            type: 'input',
+            name: 'description',
             message: questions.description
         },
-        
-        { //  installation instructions,  and test instructions
-            type: "input",
-            name: "installation",
+
+        { //installation
+            type: 'input',
+            name: 'installation',
             message: questions.installation
         },
-    
-        { //usage information
-            type: "input",
-            name: "usage",
-            message: questions.usage
-        },
-    
-        { //contribution guidelines
-            type: "input",
-            name: "contributing",
-            message: questions.contributing
-        },
-    
+
         { //usage
-            type: "input",
-            name: "usage",
+            type: 'input',
+            name: 'usage',
             message: questions.usage
         },
-    
+        
+
         { //license
             type: "checkbox", // added checkbox 
             name: "license",
-            choices: ["MIT", "GPLv3", "GPL"], /// need to pull these licenses 
+            choices: ["MIT", "GPLv3", "GPL","Apache"], /// need to pull these licenses 
             message: questions.license
         },
-    
-        { // badge
-            type: "input",
-            name: "badge",
-            message: questions.badge
+
+        { //contributing
+            type: 'input',
+            name: 'contributing',
+            message: questions.contributing
         },
-    
-        { //test
-            type: "input",
-            name: "test",
-            message: questions.test
+
+        {   //test
+            type: 'input',
+            name: 'tests',
+            message: questions.tests
         },
-    
+
+        { //email
+            type: 'input',
+            name: 'email',
+            message: questions.email
+        },
+
         { //github username 
-            type: "input",
-            name: "username",
-            message: questions.username
+            type: 'input',
+            name: 'username',
+            message: questions.github
         },
-    
-        { //github rep
+
+        { //github repo
             type: "input",
             name: "repository",
             message: questions.repository
         }
     ]);
 };
-
-     // this didn't work out 
-     // pulled from mini-project
-     //.then(response => {
-    //     console.log('response', response);
-
-    //     fs.writeFile('index.html', html, function (err) {
-    //         if (err) {
-    //             return console.log(err);
-    //         }
-    
-    //         console.log('Success! You have created a readme.');
-    //     });
-    //     })
-        
 // TODO: Create a function to write README file
-
 function writeToFile(fileName, data) {
-    fs.writeFileSync('./utils/readme.md',generateMarkdown(answers));
+    fs.writeFileSync('./output/readme.md', generateMarkdown(answers));
 }
 
-
 // TODO: Create a function to initialize app
-// Pulled this from bonus solved mini-project
- const init = () => {
-   promptUser()
-         // Use writeFileSync method to use promises instead of a callback function
-        .then((answers) => fs.writeFileSync('./utils/readme.md', generateMarkdown(answers)))
-        .then(() => console.log('Success! You created a readme.md.'))
+const init = () => { // this was pulled from bonus solved mini-project
+    promptUser()
+        // Use writeFileSync method to use promises instead of a callback function
+        .then((answers) => fs.writeFileSync('./output/readme.md', generateMarkdown(answers)))
+        .then(() => console.log('Success! You created readme.md'))
         .catch((err) => console.error(err));
- };
+};
 
 // Function call to initialize app
- init();
+init();
+
 
